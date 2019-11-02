cda328790822429287a6f48889395bc2a2f6ca34
Eric <358638369@qq.com>
2019-11-03 02:53:10 +0800

    首次提交

---
+++ deep-sea/src/test/java/com/rookie/deepsea/DeepSeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.deepsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepSeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
